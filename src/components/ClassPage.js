dda1b5ae11c3da9921d610872f076f3689ff4963
Luke G <lukegronert@gmail.com>
2021-08-31 17:16:00 +0800

    ClassPage now displays class roster in a column on the left side of the webpage.

--- src/components/ClassPage.js
+++ src/components/ClassPage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './ClassPage.css';
 
-export default function About(props) {
+export default function ClassPage({selectedClass}) {
     return (
-        <div>
-            <h1>{props.selectedClass.id}</h1>
+        <div className='container'>
+            <h1>{selectedClass.id}</h1>
+            <div className='roster'>
+                {selectedClass.roster.map((student) => {
+                    return (
+                        <div>{student.name}</div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
